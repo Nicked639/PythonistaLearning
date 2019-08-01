512b8cda75468f770ac69108a0ac04ac60a694b2
Nicktimebreak <shxnick@gmail.com>
2019-08-01 10:33:28 +0800

    mybackup

--- taotu8_jsbox3.py
+++ taotu8_jsbox3.py
@@ -1,76 +1,41 @@
-import asyncio
+import sys
+import appex
+import clipboard
+#from multiprocessing.dummy import Pool as ThreadPool
 import os
+import requests
 import time
 import webbrowser
-import aiohttp
-from progressbar import widgets, ProgressBar
-import sys
 import console
+from multiprocessing.pool import ThreadPool
+def url_response(url):
+	
+	img_name = url.split('/')[-1]
+	path = folder_path + '/' + img_name
+	r = requests.get(url, timeout=7,stream = True)
+	with open(path, 'wb') as f:
+		for ch in r:
+			f.write(ch)
+if __name__ == '__main__':
+	# 初始化
+	console.clear()
+	folder_name = sys.argv[1]
+	IMGList = sys.argv[2].split(',')
+	doc_path = os.path.join(os.path.expanduser('/private/var/mobile/Library/Mobile Documents/iCloud~com~omz-software~Pythonista3/Documents/Downloads'),'TaoTu8_Images')
+	folder_path = os.path.join(doc_path, folder_name)
+	if os.path.isdir(folder_path):
+		pass
+	else:
+		os.makedirs(folder_path)
+	start = time.time()
+	len = len(IMGList)
+	ThreadPool(20).imap_unordered(url_response, IMGList)
 
-folder_name = sys.argv[1]
-urls = sys.argv[2].split(',')
-doc_path = os.path.join(os.path.expanduser('/private/var/mobile/Library/Mobile Documents/iCloud~com~omz-software~Pythonista3/Documents/Downloads'),'TaoTu8_Images')
-folder_path = os.path.join(doc_path, folder_name)
-if not os.path.isdir(folder_path):
-  os.makedirs(folder_path)
-
-loop = asyncio.get_event_loop()
-
-
-class Progress:
-    def __init__(self, sum_exp):
-        new_widgets = [
-            widgets.Percentage(),
-            ' (', widgets.SimpleProgress(),
-            ') ', widgets.Bar(),
-            ' ', widgets.Timer(format='Elapsed Timer: %(seconds_elapsed).3fs'),
-        ]
-        self.bar = ProgressBar(max_value=sum_exp, widgets=new_widgets)
-        self.finished = 0
-        
-    def update(self, job_size) -> None:
-        self.finished += job_size
-        self.bar.update(self.finished)
-        
-    def finish(self) -> None:
-        self.bar.finish()
-        
-    def start(self) -> None:
-        self.bar.start()
+	print(f"Time to download: {time.time() - start}")
+	print('图片数量:'+str(len))
+#	time.sleep(1)
+#	webbrowser.open('jsbox://')
 
 
-async def get_url(url, session, progress):
-    file_name = url.split('/')[-1]
-    while True:
-        try:
-            with open(f'{folder_path}/{file_name}', 'wb') as fd:
-                async with session.get(url) as rsp:
-                    async for i in rsp.content:
-                        fd.write(i)
-            break
-        except Exception as e:
-            pass
-            # print(file_name, '网络异常', e.__class__.__name__)
-    progress.update(1)
-    # print(progress.finished, file_name)
 
 
-async def main():
-    console.clear()
-    len_chunck = 100
-    len_urls = len(urls)
-    
-    progress = Progress(len_urls)
-    progress.start()
-    
-    timeout = aiohttp.ClientTimeout(total=2.5)
-    async with aiohttp.ClientSession(timeout=timeout) as ss:
-        for i in range(0, len_urls, len_chunck):
-            tasks = [loop.create_task(
-                get_url(url, ss, progress)) for url in urls[i: i+len_chunck]]
-            await asyncio.wait(tasks)
-    progress.finish()
-
-
-start = time.time()
-loop.run_until_complete(main())
